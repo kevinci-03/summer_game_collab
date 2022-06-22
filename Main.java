5cf64989a68ab8c344baa41ff049fe4f4d52e978
kevincisneros <kcisne04@calpoly.edu>
2022-06-21 20:34:19 -0700

    Working Demo

--- Main.java
+++ Main.java
@@ -24,7 +24,7 @@ import com.jme3.system.AppSettings;
         @Override
         public void simpleInitApp() {
 
-            Box b = new Box(1, 1, 1);
+            Box b = new Box(10, 10, 10);
             Geometry geom = new Geometry("Box", b);
 
             Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
